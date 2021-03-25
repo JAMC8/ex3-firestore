a3bc7c3536093a08a0bdee51fb6a330c566e664a
Julian Maldonado <juliandresmc@hotmail.com>
2021-03-25 13:23:24 -0400

    description

--- src/composants/Apropos.jsx
+++ src/composants/Apropos.jsx
@@ -2,7 +2,7 @@ export default function Apropos() {
   return (
     <div className="Apropos">
       <h2>À propos de nous</h2>
-      <p>Patati et patata</p>
+      <p>Déscription à venir</p>
     </div>
   );
 }
